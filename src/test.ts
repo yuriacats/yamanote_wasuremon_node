2ef263d51924119e0c17dfcd1d369a7e7da42576
HimekaTsuchida <yuria.soft0615@gmail.com>
2022-10-02 07:50:17 +0000

    [wip]外回りのテストも書いた

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import { In_Tokyo } from "./yamanote_stations";
+import { In_Tokyo, Out_Tokyo } from "./yamanote_stations";
 
 test('山手線内回りの整合性テスト', function () {
     expect(In_Tokyo.next.name).toBe("神田")
@@ -31,4 +31,37 @@ test('山手線内回りの整合性テスト', function () {
     expect(In_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("新橋")
     expect(In_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("有楽町")
     expect(In_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("東京")
+})
+
+test('山手線外回りの整合性テスト', function () {
+    expect(Out_Tokyo.next.name).toBe("有楽町")
+    expect(Out_Tokyo.next.next.name).toBe("新橋")
+    expect(Out_Tokyo.next.next.next.name).toBe("浜松町")
+    expect(Out_Tokyo.next.next.next.next.name).toBe("田町")
+    expect(Out_Tokyo.next.next.next.next.next.name).toBe("高輪ゲートウェイ")
+    expect(Out_Tokyo.next.next.next.next.next.next.name).toBe("品川")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.name).toBe("大崎")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.name).toBe("五反田")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.name).toBe("目黒")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.name).toBe("恵比寿")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.name).toBe("渋谷")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("原宿")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("代々木")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("新宿")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("新大久保")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("高田馬場")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("目白")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("池袋")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("大塚")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("巣鴨")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("駒込")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("田端")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("西日暮里")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("日暮里")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("鶯谷")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("上野")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("御徒町")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("秋葉原")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("神田")
+    expect(Out_Tokyo.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.name).toBe("東京")
 })
