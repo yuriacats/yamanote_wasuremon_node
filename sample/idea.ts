d5e631b00fedc8cc0a241dafb95c999b5f35ad0d
HimekaTsuchida <yuria.soft0615@gmail.com>
2022-10-02 06:25:27 +0000

    update idea

--- sample/idea.ts
+++ sample/idea.ts
@@ -7,14 +7,25 @@ type date = { date: string }
 電車（始発から終着までのN週を１区切りとして）
 
 */
+// 山手線固有の番号名。（内回り、外回りで別な列車として扱う）
 type station = {
     name: string,
-    prev: station
+    prev: station,
+    station_no: number,
+    is_inside: boolean
 }
-// station __str__はnameを返す構造
+// station __str__はnameを返す構造にしたい。(内回り外回り)
+
+// 時刻表上の休日平日土曜日のカテゴライズ
+type timetable_category = "weekday" | "saturday" | "holiday";
+type time = {
+    hour: number,
+    minute: number
+}
 
 type train_loop = {
     start_station: station,
+    timetable: [time]
     end_station: station,
-
+    timetable_category: timetable_category
 }
