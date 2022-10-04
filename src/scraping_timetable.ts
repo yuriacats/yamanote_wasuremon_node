b085417df3e37f51da1de9d08cb0abc36efd94c0
HimekaTsuchida <yuria.soft0615@gmail.com>
2022-10-04 23:41:45 +0900

    idea.tsの型定義のアイデアをスクレイピングファイルに反映

--- src/scraping_timetable.ts
+++ src/scraping_timetable.ts
@@ -13,14 +13,24 @@ class screping_base {
     }
 }
 export type timetable_category = "weekday" | "saturday" | "holiday";
+// TODO 汎用的な時間型、拡張性のある汎用的なDate型を探す。（応用情報終わったら）
+type time = {
+    hour: number,
+    minits: number
+}
+type date = {
+    month: number,
+    day: number,
+    category: timetable_category
+}
 
 type station_timetable = {
     station: Station,
     timetable_category: [timetable_category],
+    station_timetable: [time],
+    date_of_expiry: date
 }
-
 
 
 const hogehuga = new screping_base("https://www.jreast-timetable.jp/2210/train/075/079411.html ")
 //TODO 山手線の一つの駅のスクレイピングを作れる様にする。
-//https://www.jreast-timetable.jp/2210/train/075/079411.html 
