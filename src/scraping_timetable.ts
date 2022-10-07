bb7f1507418dd2e61bf62287dad77029c1d23c24
HimekaTsuchida <yuria.soft0615@gmail.com>
2022-10-07 20:38:45 +0900

    Classだったものを関数に変更

--- src/scraping_timetable.ts
+++ src/scraping_timetable.ts
@@ -1,26 +1,17 @@
 import superagent from 'superagent';
 import { Station } from './yamanote_stations';
 import { JSDOM } from 'jsdom';
-import { Certificate } from 'crypto';
 
 const is_have_selector = (doc: Document, selector: string): boolean => {
     const t = doc.querySelector(selector)?.textContent
     return t ? true : false;
 }
 
-class screping_base {
-    private url: string;
-    documents;
-    constructor(url: string) {
-        this.url = url;
-        this.documents = this.getRawHtml();
-    }
-    async getRawHtml(): Promise<Document> {
-        const result = await superagent.get(this.url);
-        const dom = new JSDOM(result.text);
-        const document = dom.window.document;
-        return document;
-    }
+const get_screping_base = async (url: string): Promise<Document> => {
+    const result = await superagent.get(url);
+    const dom = new JSDOM(result.text);
+    const document = dom.window.document;
+    return document;
 }
 
 const check_loop = (target: Document): boolean => {
@@ -55,7 +46,7 @@ type station_loop = {
 
 //TODO 山手線の一つの駅のスクレイピングを作れる様にする。
 
-const huga = new screping_base("https://www.jreast-timetable.jp/2210/train/015/016491.html")
-const hoge = new screping_base("https://www.jreast-timetable.jp/2210/train/020/020431.html")
+const huga = new get_screping_base("https://www.jreast-timetable.jp/2210/train/015/016491.html")
+const hoge = new get_screping_base("https://www.jreast-timetable.jp/2210/train/020/020431.html")
 
 hoge.documents.then((val) => check_loop(val));
