a95819e745589a839acddd163c298d07eacfe89b
HimekaTsuchida <yuria.soft0615@gmail.com>
2022-10-10 21:30:30 +0900

    querySelectorの使い方を書いた

---
+++ sample/how_to_use_superAgent.ts
@@ -0,0 +1,11 @@
+// superegent/jdomの使い方をざっくりと書き留めるサンプルコードです。
+
+const only_one_tags_extract = (targetDom: Document, targetSelector: string): string => {
+    const target_text = targetDom.querySelector(targetSelector)?.textContent
+    // querySelectorはElementかNullを返す型なのでtextContentのプロパティを持ってたり持ってなかったりする。持ってない場合はNullが今回は返るはず。
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chainin
+
+    return target_text ? target_text : "";
+    // Null= false undifinde= falseなのでこれで一意にString型で帰せるので方チェックが通る。
+}
+
