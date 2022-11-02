91255dfc9b1519b2c2a86c5f65619f3148f2861a
HimekaTsuchida <yuria.soft0615@gmail.com>
2022-11-03 00:40:00 +0900

    JSDocを記載/型プロパティの更新

--- src/scraping_timetable.ts
+++ src/scraping_timetable.ts
@@ -1,15 +1,33 @@
 import superagent from 'superagent';
 import { Station } from './yamanote_stations';
 import { JSDOM } from 'jsdom';
-
+/**
+ * Selector（JSDOM方式）が引数のDocumentにあるかどうかを判定する関数です
+ * @param doc 対象のDocumentを記載。
+ * @param selector hoge classが欲しい場合は".hoge" hoge idが欲しい場合は”#hoge”と記載
+ * @return　入っている場合はTrueが返却されます。
+ */
 export const has_selector = (doc: Document, selector: string): boolean =>
     Boolean(doc.querySelector(selector)?.textContent)
-
+/**
+ * その運行テーブルの電車が環状運転かを判定する関数
+ * @param target 確認して欲しい山手線の運行テーブルページのDOMを
+ * @returns 環状運転の場合はTrueが返却されます。
+ */
 const check_yamanote_loop = (target: Document): boolean => has_selector(target, ".arrow_loop_next")
 
+/**
+ * ElementからリンクののURLを抽出する。
+ * @param elm HREFプロパティが入っていることを期待しているElement
+ * @returns URLがある場合はURLをない場合は空文字列を返します。
+ */
 export const extract_link = (elm: Element): string => elm.attributes.getNamedItem("href")?.value || "";
 
-
+/**
+ * スクレイピングしたいURLからDOM生成します。
+ * @param url 処理したいWEBサイトのURL
+ * @returns JSDOMで扱えるDOMが返却されます。
+ */
 const get_screping_base = async (url: string): Promise<Document> => {
     const result = await superagent.get(url);
     const dom = new JSDOM(result.text);
@@ -17,7 +35,13 @@ const get_screping_base = async (url: string): Promise<Document> => {
     return document;
 }
 
-// データ型にしていくタイムテーブルを作成
+/**
+ * サイトから特定のURLリンクの集合を生成する関数です。
+ * @param url サイトマップのURL
+ * @param before_url 相対パスの場合に使うURL
+ * @param target_selector サイトマップのLinkが書かれているElementの要素
+ * @returns 
+ */
 const get_saitmap_to_url_lists = async (
     url: string,
     before_url: string,
@@ -39,7 +63,6 @@ const get_train_timetabele_urls = (url: string) => get_saitmap_to_url_lists(
     ".time_link_black")
 
 export type timetable_category = "weekday" | "saturday" | "holiday";
-// TODO 汎用的な時間型、拡張性のある汎用的なDate型を探す。（応用情報終わったら）
 type time = {
     hour: number,
     minits: number
@@ -47,20 +70,19 @@ type time = {
 type date = {
     month: number,
     day: number,
-    category: timetable_category
 }
 
-type station_timetable = {
+type stationTimetable = {
     station: Station,
-    timetable_category: [timetable_category],
-    station_timetable: [time],
-    date_of_expiry: date
+    timetableCategory: [timetable_category],
+    arrivalTimes: [time],
+    dateOfExpiry: date
 }
-type station_loop = {
-    start_station: Station,
-    timetable_category: [timetable_category],
-    station_timetable: [time],
-    date_of_expiry: date
+type trainArrivalSchedule = {
+    startingStation: Station,
+    timetableCategory: [timetable_category],
+    arrivalTimes: [time],
+    dateOfExpiry: date
 }
 
 //TODO 山手線の一つの駅のスクレイピングを作れる様にする。
