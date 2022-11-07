b7ea53cf7b93a103345a5f104a1397d1ab3727b8
HimekaTsuchida <yuria.soft0615@gmail.com>
2022-11-07 20:46:45 +0900

    [add]パラメーターのバリデーションを追加

--- routes/v1.js
+++ routes/v1.js
@@ -34,32 +34,38 @@ const yamanoteStations = [
 ]
 /* GET home page. */
 router.get("/:line/:station", function (req, res, next) {
-    if (req.params.id != "in" || "out") {
-        //error を投げる
+    if (req.params.line != "in" && req.params.line != "out") {
+        res.status(404).json({
+            status: '404 not found'
+        });
     }
-    if (!yamanoteStations.includes(req.params.station)) {
-        //error　を投げる
+    else if (!yamanoteStations.includes(req.params.station)) {
+        res.status(404).json({
+            status: '404 not found'
+        });
     }
-    res.status(200).json({
-        status: "sucsess",
-        message: {
-            targetStation: "新宿",
-            arrivalTrains: [
-                {
-                    arrivelStation: "新宿",
-                    arrivelTime: "21:04",
-                },
-                {
-                    arrivelStation: "新宿",
-                    arrivelTime: "20:59",
-                },
-                {
-                    arrivelStation: "新宿",
-                    arrivelTime: "20:55",
-                },
-            ]
-        }
-    })
+    else {
+        res.status(200).json({
+            status: "sucsess",
+            message: {
+                targetStation: "新宿",
+                arrivalTrains: [
+                    {
+                        arrivelStation: "新宿",
+                        arrivelTime: "21:04",
+                    },
+                    {
+                        arrivelStation: "新宿",
+                        arrivelTime: "20:59",
+                    },
+                    {
+                        arrivelStation: "新宿",
+                        arrivelTime: "20:55",
+                    },
+                ]
+            }
+        })
+    }
 })
 router.get('/*', function (req, res, next) {
     res.status(404).json({
